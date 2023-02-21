c36efeb9b37b94312e5db7faf2307f1df074cbfd
timowandeto <timmyree16@gmail.com>
2023-02-20 16:49:30 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10 starting from 0,
+ *              followed by a new line, without any variable of type char
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar(48); /* 48 is the ASCII code for '0' */
+
+	putchar(49); /* 49 is the ASCII code for '1' */
+	
+	putchar(50); /* 50 is the ASCII code for '2' */
+	
+	putchar(51); /* 51 is the ASCII code for '3' */
+	
+	putchar(52); /* 52 is the ASCII code for '4' */
+	
+	putchar(53); /* 53 is the ASCII code for '5' */
+	
+	putchar(54); /* 54 is the ASCII code for '6' */
+	
+	putchar(55); /* 55 is the ASCII code for '7' */
+	
+	putchar(56); /* 56 is the ASCII code for '8' */
+	
+	putchar(57); /* 57 is the ASCII code for '9' */
+	
+	putchar('\n');
+
+	return (0);
+}
+
