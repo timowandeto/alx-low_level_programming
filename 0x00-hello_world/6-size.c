4e8f94481497b3c4e694477353eca6498b89bb5d
timowandeto <timmyree16@gmail.com>
2023-02-20 18:42:19 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,4 +13,21 @@ int main(void)
 
     return 0;
 }
+Size of char: 1 byte(s)
+Size of short: 2 byte(s)
+Size of int: 4 byte(s)
+Size of long: 4 byte(s)
+Size of long long: 8 byte(s)
+Size of float: 4 byte(s)
+Size of double: 8 byte(s)
+Size of long double: 12 byte(s)
+
+Size of char: 1 byte(s)
+Size of short: 2 byte(s)
+Size of int: 4 byte(s)
+Size of long: 8 byte(s)
+Size of long long: 8 byte(s)
+Size of float: 4 byte(s)
+Size of double: 8 byte(s)
+Size of long double: 16 byte(s)
 
