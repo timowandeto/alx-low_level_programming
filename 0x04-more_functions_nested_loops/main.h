a3406a2865e052836a50631862a3077cde81ad60
timowandeto <timmyree16@gmail.com>
2023-02-23 12:41:10 -0800

    main header

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * file -main.h
+ * Author: timowandeto
+ * Desc: Header contains prototypes for all functions used in 0x04-more functions more nested loops
+ *
+ */
+
+int _putchar(char c);
+
+#endif
